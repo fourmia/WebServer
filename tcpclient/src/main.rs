2def945ed0054f0a24692114862850cc2c7c4fee
fourmia <15516926476@163.com>
2024-07-17 13:16:14 +0800

    update return message.

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -6,10 +6,11 @@ use env_logger;
 
 fn main() {
     env_logger::init();
+    std::env::set_var("RUST_LOG", "info");
     let mut stream = TcpStream::connect("localhost:3000").unwrap();
     stream.write("Hello".as_bytes()).unwrap();
 
-    let mut buffer = [0; 5];
+    let mut buffer = [0; 11];
     stream.read(&mut buffer).unwrap();
     info!("Reasponse from server:{:#?}", str::from_utf8(&buffer).unwrap());
 }
--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -5,15 +5,23 @@ use env_logger;
 
 fn main() {
     env_logger::init();
+    std::env::set_var("RUST_LOG", "info");
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     info!("Running on port 3000... ");
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         info!("Connection established!");
-        let mut buffer = [0;1024];
+        let mut buffer = [0; 1024];
+        let length = stream.read(&mut buffer).unwrap();
+        info!("buffer len is:{}, length is:{}",buffer.len(),length);
+        let push_str = String::from(" rust.");
+        let push_byte = push_str.as_bytes();
+        info!("push_byte is:{}", &push_byte.len());
+        for i in length..=length+push_byte.len() {
+            buffer[i] = push_byte[i-length];
+        }
+        stream.write(&buffer[..length+push_byte.len()]).unwrap();
 
-        stream.read(&mut buffer).unwrap();
-        stream.write(&mut buffer).unwrap();
     }
     
 }
