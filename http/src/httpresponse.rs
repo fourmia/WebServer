cf44ef936e6bf64b5d4fb87f5ebecb8b88b87f6b
fourmia <15516926476@163.com>
2024-07-18 23:55:16 +0800

    update request and test, but test failed...

--- http/src/httpresponse.rs
+++ http/src/httpresponse.rs
@@ -14,3 +14,15 @@ impl From<&str> for Method {
         }
     }
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_method_into() {
+        let m: Method = "GET".into();
+        assert_eq!(m, Method::Get);
+    }
+}
